de8ce3990a2d93ecabd926b4c2ae7aef8b47dc73
zakarya525 <zakaryakhan525@gmail.com>
2023-06-09 23:17:27 +0500

    unwanted patient context removed

--- src/context/Patient/index.jsx
+++
@@ -1,5 +0,0 @@
-import patientContext from "./patientContext";
-import { useContext } from "react";
-
-export { PatientProvider } from "./PatientProvider";
-export const usePatient = () => useContext(patientContext);
--- src/context/Patient/patientContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const patientContext = createContext();
-
-export default patientContext;
